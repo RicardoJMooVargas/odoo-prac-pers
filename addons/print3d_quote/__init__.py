b005b5ef7f02287413109e44c466a435255508db
RicardoJMooVargas <al066208@uacam.mx>
2026-08-19 09:04:03 -0600

    feat: register additional mime types to prevent Odoo 500 errors on Windows

--- addons/print3d_quote/__init__.py
+++ addons/print3d_quote/__init__.py
@@ -1,2 +1,8 @@
 # -*- coding: utf-8 -*-
+import mimetypes
+
+# Fix para el error 500 al cargar iconos/fuentes en Odoo sobre Windows
+mimetypes.add_type('application/font-woff', '.woff')
+mimetypes.add_type('application/font-woff2', '.woff2')
+
 from . import models
